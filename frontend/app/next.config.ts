776838466699279e547192086cdfc8a4d0966ead
OsawaKousei <jwithelis@gmail.com>
2025-07-17 01:25:11 +0900

    fix next config

--- frontend/app/next.config.ts
+++ frontend/app/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/OsawaKousei.github.io",
-  assetPrefix: "/OsawaKousei.github.io/",
   trailingSlash: true,
   images: {
     unoptimized: true,
